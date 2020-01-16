e7310b99e3e4d723c51d47fff5b8911576dec828
Yassine El Adlouni <yeladlouni@gmail.com>
2020-01-16 11:11:27 +0100

    Add files via upload

--- src/schoolpress.php
+++ src/schoolpress.php
@@ -20,4 +20,27 @@ function sp_load_frontend_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'sp_load_frontend_styles' );
+
+function sp_load_admin_scripts() {
+	wp_enqueue_script(
+		'schoolpress-plugin-admin',
+		plugins_url( 'js/admin.js', __FILE__ ),
+		array( 'jquery' ),
+		SCHOOLPRESS_VERSION
+	);
+}
+add_action( 'admin_enqueue_scripts', 'sp_load_admin_scripts' );
+
+function sp_load_frontend_scripts() {
+	wp_enqueue_script(
+		'schoolpress-plugin-frontend',
+		plugins_url( 'js/frontend.js', __FILE__ ),
+		array( 'jquery' ),
+		SCHOOLPRESS_VERSION
+	);
+}
+add_action( 'wp_enqueue_scripts', 'sp_load_frontend_scripts' );
+
+
+
 ?>
